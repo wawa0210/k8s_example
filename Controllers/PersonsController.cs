2dab7f465701ac351ed10088370395263ba81110
LULUYOU\zhangx <xiaozhang0210@hotmail.com>
2018-06-11 15:23:04 +0800

    k8s demo

---
+++ Controllers/PersonsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace k8s_demo.Controllers
+{
+    [Route("persons")]
+    public class PersonsController : Controller
+    {
+        [HttpGet("{id}")]
+        public Person Get(int id)
+        {
+            return new Person
+            {
+                Name = "wawa",
+                Id = id
+            };
+        }
+
+        [HttpGet("")]
+        public List<Person> Get()
+        {
+            return new List<Person>()
+            {
+                new Person
+                {
+                   Name = "wawa",Id = 1
+                },
+                 new Person
+                {
+                   Name = "sally",Id = 2
+                }
+            };
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public long Id { get; set; }
+    }
+}
